a135d5845015d1a1b5a201b2b5013af357f0b0d8
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-25 08:54:40 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,2 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
+
+class Product extends React.component{
+  render() {
+    return (
+      <div>
+        <p>this.props.name</p>
+        <p>this.props.producer</p>
+        <p>this.props.hasWatermark</p>
+        <p>this.props.color</p>
+        <p>this.props.weight</p>
+      </div>
+    )
+  }
+}
+
+const colors = ['white', 'eggshell-white', 'salmon']
+
+
+Product.defaultProps = {
+  hasWatermark: false, 
+  
+}
